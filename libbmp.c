9b79212619aac5f1047e69306e429c9a66e95afa
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2016-10-11 17:37:36 +0200

    Implement the bmp_img type and use const.
    Implement the new bmp_img type and use const for some arguments.

--- libbmp.c
+++ libbmp.c
@@ -6,7 +6,7 @@
 
 /* BMP_HEADER */
 
-void bmp_header_init_df (bmp_header *header, int width, int height)
+void bmp_header_init_df (bmp_header *header, const int width, const int height)
 {
 	/* Init a bmp_header with the default values. */
 	header->bfSize = (sizeof (bmp_pixel) * width * height) + (sizeof (unsigned char) * BMP_GET_PADDING (width) * height);
@@ -25,7 +25,7 @@ void bmp_header_init_df (bmp_header *header, int width, int height)
 	header->biClrImportant = 0;
 }
 
-int bmp_header_write (bmp_header *header, FILE *img_file)
+int bmp_header_write (const bmp_header *header, FILE *img_file)
 {
 	unsigned short magic;
 	
@@ -53,21 +53,46 @@ int bmp_header_write (bmp_header *header, FILE *img_file)
 
 /* BMP_PIXEL */
 
-void bmp_pixel_init (bmp_pixel *pxl, unsigned char red, unsigned char green, unsigned char blue)
+void bmp_pixel_init (bmp_pixel *pxl, const unsigned char red, const unsigned char green, const unsigned char blue)
 {
 	pxl->red = red;
 	pxl->green = green;
 	pxl->blue = blue;
 }
 
-/* BMP_MISC */
+/* BMP_IMG */
 
-int bmp_img_write (char *filename, bmp_pixel **pxls, int width, int height)
+void bmp_img_init_df (bmp_img *img, const int width, const int height)
+{
+	int y;
+	
+	bmp_header_init_df (&img->img_header, width, height);
+	
+	/* Allocate the required memory for the pixels: */
+	img->img_pixels = (bmp_pixel**) malloc (sizeof (bmp_pixel*) * height);
+	
+	for (y = 0; y < height; y++)
+	{
+		img->img_pixels[y] = (bmp_pixel*) malloc (sizeof (bmp_pixel) * width);
+	}
+}
+
+void bmp_img_free (bmp_img *img)
+{
+	int y;
+	
+	for (y = 0; y < img->img_header.biHeight; y++)
+	{
+		free (img->img_pixels[y]);
+	}
+	free (img->img_pixels);
+}
+
+int bmp_img_write (const bmp_img *img, const char *filename)
 {
 	unsigned int x, y;
 	unsigned char padding;
 	FILE *img_file;
-	bmp_header img_header;
 	
 	img_file = fopen (filename, "wb");
 	
@@ -78,9 +103,7 @@ int bmp_img_write (char *filename, bmp_pixel **pxls, int width, int height)
 	}
 	
 	/* Write the header: */
-	bmp_header_init_df (&img_header, width, height);
-	
-	if (bmp_header_write (&img_header, img_file) != 0)
+	if (bmp_header_write (&img->img_header, img_file) != 0)
 	{
 		/* ERROR: Could'nt write the header! */
 		fclose (img_file);
@@ -90,13 +113,13 @@ int bmp_img_write (char *filename, bmp_pixel **pxls, int width, int height)
 	/* Write the content: */
 	padding = '\0';
 	
-	for (y = 0; y < height; y++)
+	for (y = 0; y < img->img_header.biHeight; y++)
 	{
 		/* Write a whole row of pixels to the file: */
-		fwrite (pxls[y], sizeof (bmp_pixel) * width, 1, img_file);
+		fwrite (img->img_pixels[y], sizeof (bmp_pixel) * img->img_header.biWidth, 1, img_file);
 		
 		/* Write the padding for the row! */
-		for (x = 0; x < BMP_GET_PADDING (width); x++)
+		for (x = 0; x < BMP_GET_PADDING (img->img_header.biWidth); x++)
 		{
 			fwrite (&padding, sizeof (padding), 1, img_file);
 		}
