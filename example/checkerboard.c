948a4e8c1369ff2027d7f00ee870c77d8a79242a
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2017-04-20 18:19:44 +0200

    Remove a tab.

--- example/checkerboard.c
+++ example/checkerboard.c
@@ -9,7 +9,7 @@ main (int argc, char *argv[])
 
 	// Draw a checkerboard pattern:
 	for (size_t y = 0; y < 512; y++)
-	{	
+	{
 		for (size_t x = 0; x < 512; x++)
 		{
 			if ((y % 128 < 64 && x % 128 < 64) ||
