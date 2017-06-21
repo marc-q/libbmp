add18e954324e790f6632e212c0f027ba0f0ca76
Marc Volker Dickmann <marc-q@users.noreply.github.com>
2017-06-21 16:50:04 +0200

    Update libbmp.h
    Update the code style to support narrower lines.

--- libbmp.h
+++ libbmp.h
@@ -14,56 +14,66 @@ enum bmp_error
 	BMP_OK = 0
 };
 
-struct _bmp_header
+typedef struct _bmp_header
 {
-	unsigned int bfSize;
-	unsigned int bfReserved;
-	unsigned int bfOffBits;
-	unsigned int biSize;
-	int biWidth;
-	int biHeight;
+	unsigned int   bfSize;
+	unsigned int   bfReserved;
+	unsigned int   bfOffBits;
+	
+	unsigned int   biSize;
+	int            biWidth;
+	int            biHeight;
 	unsigned short biPlanes;
 	unsigned short biBitCount;
-	unsigned int biCompression;
-	unsigned int biSizeImage;
-	int biXPelsPerMeter;
-	int biYPelsPerMeter;
-	unsigned int biClrUsed;
-	unsigned int biClrImportant;
-};
+	unsigned int   biCompression;
+	unsigned int   biSizeImage;
+	int            biXPelsPerMeter;
+	int            biYPelsPerMeter;
+	unsigned int   biClrUsed;
+	unsigned int   biClrImportant;
+} bmp_header;
 
-typedef struct _bmp_header bmp_header;
-
-struct _bmp_pixel
+typedef struct _bmp_pixel
 {
 	unsigned char blue;
 	unsigned char green;
 	unsigned char red;
-};
+} bmp_pixel;
 
-typedef struct _bmp_pixel bmp_pixel;
-
-struct _bmp_img
+typedef struct _bmp_img
 {
-	bmp_header img_header;
-	bmp_pixel **img_pixels;
-};
-
-typedef struct _bmp_img bmp_img;
+	bmp_header   img_header;
+	bmp_pixel  **img_pixels;
+} bmp_img;
 
 // BMP_HEADER
-void bmp_header_init_df (bmp_header*, const int, const int);
-enum bmp_error bmp_header_write (const bmp_header*, FILE*);
-enum bmp_error bmp_header_read (bmp_header*, FILE*);
+void            bmp_header_init_df             (bmp_header*,
+                                                const int,
+                                                const int);
+
+enum bmp_error  bmp_header_write               (const bmp_header*,
+                                                FILE*);
+
+enum bmp_error  bmp_header_read                (bmp_header*,
+                                                FILE*);
 
 // BMP_PIXEL
-void bmp_pixel_init (bmp_pixel*, const unsigned char, const unsigned char, const unsigned char);
+void            bmp_pixel_init                 (bmp_pixel*,
+                                                const unsigned char,
+                                                const unsigned char,
+                                                const unsigned char);
 
 // BMP_IMG
-void bmp_img_alloc (bmp_img*);
-void bmp_img_init_df (bmp_img*, const int, const int);
-void bmp_img_free (bmp_img*);
-enum bmp_error bmp_img_write (const bmp_img*, const char*);
-enum bmp_error bmp_img_read (bmp_img*, const char*);
+void            bmp_img_alloc                  (bmp_img*);
+void            bmp_img_init_df                (bmp_img*,
+                                                const int,
+                                                const int);
+void            bmp_img_free                   (bmp_img*);
+
+enum bmp_error  bmp_img_write                  (const bmp_img*,
+                                                const char*);
+
+enum bmp_error  bmp_img_read                   (bmp_img*,
+                                                const char*);
 
 #endif /* __LIBBMP_H__ */
